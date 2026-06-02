3d2fd43c37eabcbca2af60a2045d99604adf86e6
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-06-02 17:58:28 +0200

    change name of add-job route

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl font-bold">Job Search</h1>
+      <h1 className="text-2xl font-bold">Hitta din tech stack</h1>
       <nav className="mt-2">
         <ul className="flex space-x-4">
           <li>
             <Link href="/">HEM</Link>
           </li>
           <li>
-            <Link href="/add-job">LÄGG TILL FÖRETAG</Link>
+            <Link href="/add-company">LÄGG TILL FÖRETAG</Link>
           </li>
           <li>
             <Link href="/add-tech">LÄGG TILL TECH</Link>
